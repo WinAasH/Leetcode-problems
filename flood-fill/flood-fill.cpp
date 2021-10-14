e9836fc212893a6b78b3c17e631cb599f1a18fcd
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-10-15 01:20:34 +0530

    Time: 32 ms (5.69%), Space: 14.4 MB (17.29%) - LeetHub

--- flood-fill/flood-fill.cpp
+++ flood-fill/flood-fill.cpp
@@ -1,32 +1,23 @@
 class Solution {
 private:
-    void dfs(vector<vector<int>> &v, int sRow, int sColumn, int newColor, int r, int c, int s){
-        if(sRow<0  ||  sRow>=r  ||  sColumn<0  ||  sColumn>=c){
-            return;
-        }
-        if(v[sRow][sColumn]!= s){
+    void fill(vector<vector<int>> &img, int r, int c, int oldColor, int color, vector<vector<int>> &vis){
+        if(r<0  ||  c<0  ||  r==img.size()  ||  c==img[0].size()  ||  vis[r][c]  ||  img[r][c]!=oldColor){
             return;
         }
         
-        v[sRow][sColumn]= newColor;
-        
-        dfs(v, sRow+1, sColumn, newColor, r, c, s);
-        dfs(v, sRow-1, sColumn, newColor, r, c, s);
-        dfs(v, sRow, sColumn+1, newColor, r, c, s);
-        dfs(v, sRow, sColumn-1, newColor, r, c, s);
+        vis[r][c]= 1;
+        img[r][c]= color;
+        fill(img, r+1, c, oldColor, color, vis);
+        fill(img, r, c+1, oldColor, color, vis);
+        fill(img, r-1, c, oldColor, color, vis);
+        fill(img, r, c-1, oldColor, color, vis);
     }
-    
-    
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int source= image[sr][sc];
-        if(newColor== source){
-            return image;
-        }
-        
-        int row= image.size();
-        int column= image[0].size();
-        dfs(image, sr, sc, newColor, row, column, source);
+        int n= image.size(), m= image[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        int oldColor= image[sr][sc];
+        fill(image, sr, sc, oldColor, newColor, visited);
         return image;
     }
 };
