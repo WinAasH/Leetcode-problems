08b5f77aa006602953e24f8ec49323aa3a8ada2a
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-10-16 01:45:47 +0530

    Time: 12 ms (96.77%), Space: 23.2 MB (68.68%) - LeetHub

--- max-area-of-island/max-area-of-island.cpp
+++ max-area-of-island/max-area-of-island.cpp
@@ -1,21 +1,22 @@
 class Solution {
 private:
-    int countArea(vector<vector<int>> &v, int r, int c, int row, int column){
-        if(r<0  ||  r>=row  ||  c<0  ||  c>=column  ||  !v[r][c]){
+    int area(vector<vector<int>> &grid, int r, int c){
+        if(r<0  ||  c<0  ||  r==grid.size()  ||  c==grid[0].size()  ||  !grid[r][c]){
             return 0;
         }
-        v[r][c]= 0;
-        return (1+ countArea(v, r+1, c, row, column)+ countArea(v, r-1, c, row, column)+ countArea(v, r, c+1, row, column)+ countArea(v, r, c-1, row, column));
+        
+        grid[r][c]= 0;
+        return 1+ area(grid, r+1, c)+ area(grid, r-1, c)+ area(grid, r, c+1)+ area(grid, r, c-1);
     }
-    
+
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int row= grid.size(), column= grid[0].size();
-        int maxArea= 0;
-        for(int i=0; i<row; i++){
-            for(int j=0; j<column; j++){
+        int maxArea=0;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
                 if(grid[i][j]){
-                    maxArea= max(countArea(grid, i, j, row, column), maxArea);
+                    int totalArea= area(grid, i, j);
+                    maxArea= max(maxArea, totalArea);
                 }
             }
         }
