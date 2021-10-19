8d13411f01fb3bbdc39c4adbcd66b7f6fc294e89
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-10-19 16:25:21 +0530

    Time: 7 ms (63.29%), Space: 8.9 MB (31.90%) - LeetHub

--- next-greater-element-i/next-greater-element-i.cpp
+++ next-greater-element-i/next-greater-element-i.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& n1, vector<int>& n2) {
         int l1= n1.size(), l2= n2.size();
-        vector<int> nextGreater(l2);
+        unordered_map<int, int> nextGreater;
         stack<int> st;
         for(int i=l2-1; i>=0; i--){
             while(!st.empty()  &&  st.top()<=n2[i]){
                 st.pop();
             }
             if(st.empty()){
-                nextGreater[i]= -1;
+                nextGreater[n2[i]]= -1;
             }
             else{
-                nextGreater[i]= st.top();
+                nextGreater[n2[i]]= st.top();
             }
             st.push(n2[i]);
         }
         
         for(int i=0; i<l1; i++){
-            for(int j=0; j<l2; j++){
-                if(n1[i]==n2[j]){
-                    n1[i]= nextGreater[j];
-                    break;
-                }
-            }
+            auto it= nextGreater.find(n1[i]);
+            n1[i]= it->second;
         }
         
         return n1;
