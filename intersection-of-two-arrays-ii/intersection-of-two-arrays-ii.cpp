e465cbd9d99d84eabe17c9455dcf71b411ec42f1
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-10-19 17:25:32 +0530

    Time: 8 ms (49.87%), Space: 10.8 MB (15.27%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,22 +1,21 @@
 class Solution {
 private:
     vector<int> getFrequency(vector<int> &v){
-        vector<int> a(1001, 0);
+        vector<int> res(1001, 0);
         for(int i=0; i<v.size(); i++){
-            a[v[i]]++;
+            res[v[i]]++;
         }
-        return a;
+        return res;
     }
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> frequency1= getFrequency(nums1);
-        
-        vector<int> frequency2= getFrequency(nums2);
+        vector<int> f1= getFrequency(nums1);
+        vector<int> f2= getFrequency(nums2);
         
         vector<int> ans;
-        for(int i=0; i<frequency1.size(); i++){
-            int count= min(frequency1[i], frequency2[i]);
-            while(count>0){
+        for(int i=0; i<f1.size(); i++){
+            int count= min(f1[i], f2[i]);
+            while(count){
                 ans.push_back(i);
                 count--;
             }
