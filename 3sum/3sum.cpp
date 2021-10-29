7be46e7dc2dbb87fad58c918aac3804cf222e56a
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-10-30 01:13:53 +0530

    Time: 76 ms (76.85%), Space: 20 MB (66.12%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
         int n= nums.size();
+        vector<vector<int>> ans;
         
-        for(int i=0; i<n-1; i++){
-            if(i>0  &&  nums[i-1]==nums[i]){
+        for(int i=0; i<n-1  &&  nums[i]<=0; i++){
+            if(i>0  &&  nums[i]==nums[i-1]){
                 continue;
             }
-            int start= i+1, end= n-1;
             
-            while(start<end){
-                int Sum= nums[i]+nums[start]+nums[end];
-                if(Sum==0){
+            int start= i+1, end= n-1;
+            while(start < end){
+                if(nums[i]+nums[start]+nums[end]==0){
                     ans.push_back({nums[i], nums[start], nums[end]});
                     start++;
                     while(start<end  &&  nums[start]==nums[start-1]){
@@ -24,17 +23,11 @@ public:
                         end--;
                     }
                 }
-                else if(Sum<0){
+                else if(nums[i]+nums[start]+nums[end] < 0){
                     start++;
-                    while(start<end  &&  nums[start]==nums[start-1]){
-                        start++;
-                    }
                 }
                 else{
                     end--;
-                    while(start<end  &&  nums[end]==nums[end+1]){
-                        end--;
-                    }
                 }
             }
         }
