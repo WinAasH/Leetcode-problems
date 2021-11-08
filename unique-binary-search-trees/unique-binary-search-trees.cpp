6da18eae7ff980ac8b2ddd16688da3c6fc6b5278
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-11-08 21:17:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.02%) - LeetHub

---
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dp[20]= {0};
+    int numTrees(int n) {
+        if(n<=1){
+            return 1;
+        }
+        
+        if(dp[n]>0){
+            return dp[n];
+        }
+        
+        for(int i=1; i<=n; i++){
+            dp[n]+= numTrees(n-i)*numTrees(i-1);
+        }
+        return dp[n];
+    }
+};
