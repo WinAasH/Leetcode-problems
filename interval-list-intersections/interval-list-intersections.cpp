6a81fa3c2183ab09ffae5359eaa249090f03ade6
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-11-24 21:23:27 +0530

    Time: 28 ms (90.94%), Space: 18.6 MB (75.13%) - LeetHub

--- interval-list-intersections/interval-list-intersections.cpp
+++ interval-list-intersections/interval-list-intersections.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& L1, vector<vector<int>>& L2) {
-        int n1= L1.size(), n2= L2.size();
+        int len1= L1.size(), len2= L2.size();
         vector<vector<int>> ans;
-        if(n1==0  ||  n2==0) return ans;
+        if(len1==0  ||  len2==0) return ans;
         
         int first= 0, second= 0;
-        while(first<n1  &&  second<n2){
-            if(L1[first][0]<=L2[second][1]  &&  L1[first][1]>=L2[second][0]){
+        while(first<len1  &&  second<len2){
+            if(L1[first][1]>=L2[second][0]  &&  L1[first][0]<=L2[second][1]){
                 ans.push_back({max(L1[first][0], L2[second][0]), min(L1[first][1], L2[second][1])});
             }
-            else if(L2[second][1]>=L1[first][1]  &&  L1[first][0]>=L2[second][0]){
-                ans.push_back({max(L1[first][0], L2[second][0]), min(L1[first][1], L2[second][1])});
+            else if(L1[first][1]>=L2[second][1]  &&  L1[first][0]<=L2[second][0]){
+                ans.push_back({L2[second][0], L2[second][1]});
             }
             
             if(L1[first][1]>L2[second][1]) second++;
