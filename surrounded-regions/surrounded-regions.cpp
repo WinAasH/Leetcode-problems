c6685996f5c0a3828345ca781d2bcfedb023e579
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-11-27 23:23:35 +0530

    Time: 16 ms (28.66%), Space: 14.7 MB (11.24%) - LeetHub

--- surrounded-regions/surrounded-regions.cpp
+++ surrounded-regions/surrounded-regions.cpp
@@ -1,44 +1,41 @@
 class Solution {
 private:
-    void change(vector<vector<char>> &board, int row, int col, char ch, vector<vector<int>> &vis){
-        if(row<0  ||  col<0  ||  row>=board.size()  ||  col>=board[0].size()  ||  board[row][col]=='X'  ||  vis[row][col]==1){
-            return ;
-        }
-        vis[row][col]=1;
-        board[row][col]= ch;
-        change(board, row+1, col, ch, vis);
-        change(board, row-1, col, ch, vis);
-        change(board, row, col+1, ch, vis);
-        change(board, row, col-1, ch, vis);
+    void change(vector<vector<char>> &v, int r, int c, char ch, char ch2){
+        if(r>=v.size()  ||  c>=v[0].size()  ||  r<0  ||  c<0  ||  v[r][c]!=ch2) return;
+        
+        v[r][c]= ch;
+        change(v, r+1, c, ch, ch2);
+        change(v, r-1, c, ch, ch2);
+        change(v, r, c+1, ch, ch2);
+        change(v, r, c-1, ch, ch2);
         return;
     }
-    
 public:
     void solve(vector<vector<char>>& board) {
         int row= board.size(), col= board[0].size();
-        vector<vector<int>> visited(row, vector<int>(col, 0));
-        for(int i=0; i<row; i++){
-            if(visited[i][0]==0  &&  board[i][0]=='O'){
-                change(board, i, 0, 'Y', visited);
+        
+        for(int i=0; i<col; i++){
+            if(board[0][i]=='O'){
+                change(board, 0, i, 'Y', 'O');
             }
-            if(visited[i][col-1]==0  &&  board[i][col-1]=='O'){
-                change(board, i, col-1, 'Y', visited);
+            if(board[row-1][i]=='O'){
+                change(board, row-1, i, 'Y', 'O');
             }
         }
         
-        for(int i=0; i<col; i++){
-            if(visited[0][i]==0  &&  board[0][i]=='O'){
-                change(board, 0, i, 'Y', visited);
+        for(int i=0; i<row; i++){
+            if(board[i][0]=='O'){
+                change(board, i, 0, 'Y', 'O');
             }
-            if(visited[row-1][i]==0  &&  board[row-1][i]=='O'){
-                change(board, row-1, i, 'Y', visited);
+            if(board[i][col-1]=='O'){
+                change(board, i, col-1, 'Y', 'O');
             }
         }
         
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 if(board[i][j]=='O'){
-                    board[i][j]= 'X';
+                    change(board, i, j, 'X', 'O');
                 }
             }
         }
@@ -46,7 +43,7 @@ public:
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 if(board[i][j]=='Y'){
-                    board[i][j]= 'O';
+                    change(board, i, j, 'O', 'Y');
                 }
             }
         }
