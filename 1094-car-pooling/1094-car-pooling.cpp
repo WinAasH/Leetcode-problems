4d023b10b5963503c6e8e87d14bc394ef2acdbc1
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-01-06 23:21:22 +0530

    Time: 8 ms (95.97%), Space: 10.4 MB (39.70%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int curr= 0;
+        if(trips[0][0]>capacity) return false;
+        map<int, int> mp;
+        for(int i=0; i<trips.size(); i++){
+            mp[trips[i][1]]+= trips[i][0];
+            mp[trips[i][2]]-= trips[i][0];
+        }
+        for(auto it: mp){
+            curr+= it.second;
+            if(curr>capacity)   return false;
+        }
+        return true;
+    }
+};
