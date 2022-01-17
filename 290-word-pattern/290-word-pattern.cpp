576bcbf14409daaf6de561b10b11d5d838495f5f
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-01-18 00:37:04 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (18.34%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        if(pattern.length()==0  ||  s.length()==0){
-            return false;
-        }
-        
-        vector<string> str;
+        vector<string> v;
         string temp= "";
         for(char ch: s){
             if(ch==' '){
-                str.push_back(temp);
+                v.push_back(temp);
                 temp= "";
             }
-            else{
-                temp+= ch;
-            }
+            else    temp+= ch;
         }
-        str.push_back(temp);
-        
-        if(pattern.length()!=str.size()){
-            return false;
+        v.push_back(temp);
+        if(v.size()!=pattern.length())  return false;
+        map<char, string> mp;
+        for(int i=0; i<v.size(); i++){
+            if(mp.find(pattern[i])!=mp.end()){
+                string curr= mp[pattern[i]];
+                if(curr!=v[i])  return false;
+            }
+            else    mp[pattern[i]]= v[i];
         }
-        
-        map<char, string> charMap;
-        map<string, char> stringMap;
-        for(int i=0; i<str.size(); i++){
-            if(charMap.find(pattern[i])==charMap.end()){
-                charMap[pattern[i]]= str[i];
+        map<string, char> mp2;
+        for(int i=0; i<v.size(); i++){
+            if(mp2.find(v[i])!=mp2.end()){
+                char curr= mp2[v[i]];
+                if(curr!=pattern[i])    return false;
             }
-            else{
-                if(charMap[pattern[i]]!=str[i]){
-                    return false;
-                }
-            }
-            
-            if(stringMap.find(str[i])==stringMap.end()){
-                stringMap[str[i]]= pattern[i];
-            }
-            else{
-                if(stringMap[str[i]]!=pattern[i]){
-                    return false;
-                }
-            }
+            else    mp2[v[i]]= pattern[i];
         }
-        
         return true;
     }
 };
