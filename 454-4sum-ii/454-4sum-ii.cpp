7fb7c988ecf32f6b93711c026f18689f867bba5c
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-02-04 03:11:31 +0530

    Time: 416 ms (25.74%), Space: 24.8 MB (27.40%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -3,11 +3,7 @@ public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         int ans= 0;
         map<int, int> mp;
-        for(int i=0; i<nums1.size(); i++){
-            for(int j=0; j<nums2.size(); j++){
-                mp[nums1[i]+nums2[j]]++;
-            }
-        }
+        for(int i=0; i<nums1.size(); i++)   for(int j=0; j<nums2.size(); j++)   mp[nums1[i]+nums2[j]]++;
         for(int i=0; i<nums3.size(); i++){
             for(int j=0; j<nums4.size(); j++){
                 int toFind= (nums3[i]+nums4[j])*(-1);
