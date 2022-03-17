b34aebccaad136b42c78866621fef313d0e6cabd
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-03-17 05:44:34 +0530

    Time: 5 ms (14.59%), Space: 6.2 MB (8.12%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        if(s == "()") return 1;
+        
+        int i = 0, j = 1, cnt = 1, score = 0;
+        while(i < s.length()) {
+            while(j < s.length() and cnt) {
+                if(s[j] == '(') cnt++;
+                else cnt--;
+                j++;
+            }
+            
+            if(s.substr(i,j-i) == "()") score++; 
+            else score += 2*scoreOfParentheses(s.substr(i+1,j-i-2));
+            i = j++;
+            cnt = 1;
+        }
+        
+        return score;
+    }
+};
