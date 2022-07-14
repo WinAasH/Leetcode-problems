e245da5369836c2b14f863261dad836734386959
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-07-14 16:32:43 +0530

    Time: 19 ms (88.36%), Space: 26.4 MB (47.35%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,38 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+private:
+    TreeNode* helper(vector<int> &preorder, int preS, int preE, vector<int> &inorder, int inS, int inE, map<int, int> &mp){
+        if(preS>preE  ||  inS>inE)  return NULL;
+        
+        TreeNode *curr= new TreeNode(preorder[preS]);
+        
+        int n= mp[preorder[preS]];
+        
+        curr->left= helper(preorder, preS+1, preS+n-inS, inorder, inS, n-1, mp);
+        curr->right= helper(preorder, preS+n-inS+1, preE, inorder, n+1, inE, mp);
+        return curr;
+    }
 public:
-    TreeNode* helperTree(vector<int> &in,vector<int> &pre,int inS,int inE,int preS,int preE){
-    //empty base case
-    if(inS > inE){
-        return NULL;
-    }
-    int rootData = pre[preS];
-    int rootindex = -1;
-    for(int i=inS; i<=inE; ++i){
-        if(in[i] == rootData){
-            rootindex = i;
-        	break;
-        }
-    }
-    //some variables
-    int linS = inS;
-    int linE = rootindex-1;
-    int lpreS = preS+1;
-    int lpreE = linE-linS+lpreS;
-  
-    int rpreS = lpreE+1;
-    int rpreE = preE;
-    int rinS = rootindex+1;
-    int rinE = inE;
-    
-    TreeNode* root = new TreeNode(rootData);
-    root->left = helperTree(in,pre,linS,linE,lpreS,lpreE);
-    root->right = helperTree(in,pre,rinS,rinE,rpreS,rpreE);
-    return root;
-    
-}
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int inLength = inorder.size();
-        int preLength = preorder.size();
-        return helperTree(inorder,preorder,0,inLength-1,0,preLength-1);
+        map<int, int> mp;
+        
+        for(int i=0; i<preorder.size(); i++){
+            mp[inorder[i]]= i;
+        }
+        
+        TreeNode *ans= helper(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, mp);
+        return ans;
     }
 };
