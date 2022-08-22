3051f9a55b89ab41ee17ff9bb9b59fc688f6fdc6
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-08-22 05:32:12 +0530

    Time: 42 ms (52.46%), Space: 11.2 MB (42.62%) - LeetHub

--- 936-stamping-the-sequence/936-stamping-the-sequence.cpp
+++ 936-stamping-the-sequence/936-stamping-the-sequence.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    
-    // match two strings along with wildcard character '?'
-    // '?' matches with all characters (e.g. abc == a?c)
     bool match(string &a,string &b){
         if(a.length() != b.length()) 
             return false;
         for(int i=0;i<a.length();i++)
-            if(a.at(i) != b.at(i) && a.at(i) != '?' && b.at(i) != '?') 
+            if(a.at(i) != b.at(i) && b.at(i) != '?') 
                 return false;
         return true;
     }
     
-    // check if the string is all wildcard characters
     bool allWild(string &s){
         for(int i=0;i<s.length();i++)
             if(s.at(i) != '?') 
@@ -25,31 +21,21 @@ public:
         vector<int> ans;
         string str ; 
         bool foundMatch = true;
-        
-        // loop until no more reverse stamping can be done
         while(foundMatch){ 
-            foundMatch = false; // reset flag
+            foundMatch = false;
             for(int i=0;i<n-m+1;i++){
-                str = target.substr(i,m);  // get substr of length m
-                
-                // if its all windcard no need to stamp
+                str = target.substr(i,m);            
                 if(allWild(str)) continue; 
-                if(match(stamp,str)){  // match substr with stamp
-                    foundMatch = true; // set flag
-                    ans.push_back(i); // push the i as match is found
-                    // replace all chars of str in target to '?'
+                if(match(stamp,str)){
+                    foundMatch = true;
+                    ans.push_back(i);
                     for(int j=0;j<m;j++) target[i+j] = '?'; 
                 }
             }
         }
-        
-        // base case
         if(!allWild(target))  ans.clear();
         if(ans.size() > 10*n) ans.clear();
-        
-        // reverse answer as we did the whole procedure in reverse
         reverse(ans.begin(),ans.end());
-        
         return ans;
     }
 };
