6cf5f4323b1a2c9835ee946fd315bb940b77bdc5
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-08-25 04:05:12 +0530

    Time: 45 ms (99.94%), Space: 21.7 MB (99.93%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    void swap(vector<int> &arr,int a,int b){
-        int temp=arr[a];
-        arr[a]=arr[b];
-        arr[b]=temp;
-    }
     int firstMissingPositive(vector<int>& arr) {
         int l=arr.size();
         for(int i=0;i<l;i++){
-            // for(int i=0;i<l;i++){
-            //     cout<<arr[i]<<" ";
-            // }
-            // cout<<endl;
-            if(arr[i]>=l||arr[i]<=0||arr[i]==i){
-                
-            }
+            if(arr[i]>=l  ||  arr[i]<=0  ||  arr[i]==i  ||  arr[arr[i]]==arr[i])    continue;
             else{
-                if(arr[arr[i]]==arr[i]) continue;
-                // cout<<"here";
-                swap(arr,i,arr[i]);
+                int temp=arr[arr[i]];
+                arr[arr[i]]=arr[i];
+                arr[i]=temp;
                 i--;
             }
         }
-        
-        for(int i=1;i<l;i++){
-            if(arr[i]!=i){
-                return i;
-            }
-        }
+        for(int i=1;i<l;i++)    if(arr[i]!=i)   return i;
         if(arr[0]==l) return l+1;
         return l;
     }
