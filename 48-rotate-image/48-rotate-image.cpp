44f2a3aa08afa04e2074b2019fcc85c8f7f79595
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-08-31 01:47:29 +0530

    Time: 0 ms (100.00%), Space: 7 MB (73.72%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rot(vector<vector<int>> &arr,int k,int n){
+        int r=n-1-k;
+        int length=r-k+1;
+        for(int i=0;i<length-1;i++){
+            int t1=arr[k][k+i];
+            int t2=arr[k+i][k+length-1];
+            int t3=arr[k+length-1][k+length-1-i];
+            int t4=arr[k+length-1-i][k];
+            arr[k][k+i]=t4;
+            arr[k+i][k+length-1]=t1;
+            arr[k+length-1][k+length-1-i]=t2;
+            arr[k+length-1-i][k]=t3;
+            
+        }
+    }
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix[0].size();
+        cout<<n;
+        for(int i=0;i<=(n-1)/2;i++){
+            rot(matrix,i,n);
+        }
+    }
+};          
