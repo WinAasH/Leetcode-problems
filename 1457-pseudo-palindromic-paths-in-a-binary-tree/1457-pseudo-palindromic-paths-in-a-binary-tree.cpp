ef05709ea23d7daee16403998ff7f55627347cb9
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-09-14 15:17:53 +0530

    Time: 354 ms (98.20%), Space: 183.3 MB (51.25%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -10,21 +10,19 @@
  * };
  */
 class Solution {
-public:
-    
-    int dfs(TreeNode *root , int a[]) {
-        if(root==NULL) return 0;
+private:
+    int dfs(TreeNode *root, vector<int> &a){
+        if(root==NULL)  return 0;
         a[root->val]++;
-        int ans= dfs(root->left,a) + dfs(root->right,a) , odd =0;
-        for(int i=1;i<=9;i++) if(a[i]%2) odd++;
+        int ans= dfs(root->left, a)+ dfs(root->right, a), odd= 0;
+        for(int i=1; i<=9; i++)  if(a[i]%2)  odd++;
         a[root->val]--;
-        if(root->right==NULL and root->left==NULL and odd<=1) ans++;
+        if(root->left==NULL  &&  root->right==NULL  &&  odd<=1) ans++;
         return ans;
     }
-    
+public:
     int pseudoPalindromicPaths (TreeNode* root) {
-        int a[10];
-        memset(a,0,sizeof(a));
-        return dfs(root,a);
+        vector<int> a(10, 0);
+        return dfs(root, a);
     }
 };
