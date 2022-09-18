e4d012266ce470111fdc26dbf73f6939a69fa695
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-09-18 16:13:41 +0530

    Time: 33 ms (29.82%), Space: 20.6 MB (25.17%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -5,28 +5,15 @@ private:
     }
 public:
     int trap(vector<int>& height) {
-        long long n= height.size();
-        vector<long long> l(n, INT_MIN), r(n, INT_MIN);
+        int n= height.size();
+        vector<int> l(n, INT_MIN), r(n, INT_MIN);
         l[0]= height[0];
         r[n-1]= height[n-1];
-        for(int i=1; i<n; i++){
-            l[i]= max(l[i-1], height[i]);
-        }
-        for(int i=0; i<n; i++){
-            cout<< l[i]<< " ";
-        }
-        cout<< "\n";
-        for(int i=n-2; i>=0; i--){
-            r[i]= max(r[i+1], height[i]);
-        }
-        for(int i=0; i<n; i++){
-            cout<< r[i]<< " ";
-        }
-        cout<< "\n";
-        long long ans= 0;
+        for(int i=1; i<n; i++)  l[i]= max(l[i-1], height[i]);
+        for(int i=n-2; i>=0; i--)   r[i]= max(r[i+1], height[i]);
+        int ans= 0;
         for(int i=1; i<n-1; i++){
             int temp= min(l[i], r[i]);
-            cout<< temp<< " ";
             ans+= temp-height[i];
         }
         return ans;
