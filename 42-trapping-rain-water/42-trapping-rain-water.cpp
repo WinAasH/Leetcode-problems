dc42f0de357ae4294387732cbd6b88b6b1c93747
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-09-18 16:12:35 +0530

    Time: 264 ms (5.15%), Space: 21.3 MB (5.34%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,29 +1,34 @@
 class Solution {
+private:
+    int max(int a, int b){
+        return a>b?a:b;
+    }
 public:
     int trap(vector<int>& height) {
-        stack<pair<int,int>> st;
-        st.push({height[0],0});
-        int ans=0;
-        for(int i=1;i<height.size();i++){
-            cout<<ans<<" ";
-            if(!st.empty()  &&  height[i]<=st.top().first){
-                st.push({height[i],i});
-            }
-            else{
-                while(!st.empty()&&st.top().first<height[i]){
-                    pair<int,int> temp=st.top();
-                    st.pop();
-                    int width;
-                    if(!st.empty()) width=i-st.top().second-1;
-                    else width=0;
-                    int heightc=0;
-                    if(!st.empty()) heightc=min(height[i],st.top().first)-temp.first;
-                    ans+=heightc*width;
-                }
-                st.push({height[i],i});
-            }
+        long long n= height.size();
+        vector<long long> l(n, INT_MIN), r(n, INT_MIN);
+        l[0]= height[0];
+        r[n-1]= height[n-1];
+        for(int i=1; i<n; i++){
+            l[i]= max(l[i-1], height[i]);
+        }
+        for(int i=0; i<n; i++){
+            cout<< l[i]<< " ";
+        }
+        cout<< "\n";
+        for(int i=n-2; i>=0; i--){
+            r[i]= max(r[i+1], height[i]);
+        }
+        for(int i=0; i<n; i++){
+            cout<< r[i]<< " ";
+        }
+        cout<< "\n";
+        long long ans= 0;
+        for(int i=1; i<n-1; i++){
+            int temp= min(l[i], r[i]);
+            cout<< temp<< " ";
+            ans+= temp-height[i];
         }
         return ans;
     }
-
 };
