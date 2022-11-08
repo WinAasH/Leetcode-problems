13ef9286c604201ffad6a457845ed94f18b194c2
Aashwin Tripathi <aashwinfeind@gmail.com>
2022-11-08 16:42:50 +0530

    Time: 14 ms (5.98%), Space: 7.4 MB (5.40%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,18 +1,30 @@
+// class Solution {
+// public:
+//     string makeGood(string s) {
+//         if(s.empty()){
+//             return s;
+//         }
+//         int n= s.length();
+//         for(int i=0; i<n-1; i++){
+//             if(abs(s[i]-s[i+1]==32)){
+//                 return makeGood(s.substr(0, i)+ s.substr(i+2));
+//             }
+//         }
+//         return s;
+//     }
+// };
+
 class Solution {
 public:
     string makeGood(string s) {  
-        // If the string is emtpy, just return it.
-        if (s.empty()) 
+        if(s.empty()){ 
             return s;
-        
-        // If we find a pair in 's', remove this pair from 's'
-        // and solve the remaining string recursively.
-        for (int i = 0; i < s.size() - 1; ++i) {
+        }
+        int n= s.length();
+        for (int i = 0; i < n - 1; ++i) {
             if (abs(s[i] - s[i + 1]) == 32)
                 return  makeGood(s.substr(0, i) + s.substr(i + 2));
         }
-
-        // Base case, if we can't find a pair, just return 's'.
         return s;
     }
 };
